d7d2d4dea1e027ca0abc2a63b2c16d128e36954c
Sam DeKlyen <samdeklyen@gmail.com>
2019-10-09 11:44:36 -0400

    api call

---
+++ src/app/apicall.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+import { Observable } from 'rxjs';
+
+import {Jobs} from './jobs';
+
+Injectable({
+    providedIn: 'root'
+})
+
+export class JobBoardAPI {
+
+    private jobBoardURL = 'https://authenticjobs.com/api/?api_key=cf1acfdb39dea5fc2e2ea6b394c2a247&method=aj.jobs.search&perpage=100&format=json'
+
+    httpOptions = {
+        headers: new HttpHeaders({ 'Content-Type': 'Application/json'})
+    };
+
+    constructor(
+        private http: HttpClient
+    ) {}
+    
+    getJobs(): Observable<Jobs[]> {
+        return this.http.get<Jobs[]>(this.jobBoardURL);
+    }
+}
