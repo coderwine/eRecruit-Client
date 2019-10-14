7b3b0b7eaeaa5e0af1bef3467d01ec82b92e9390
Rudi Rudicel <alexrudicel@comcast.net>
2019-10-14 14:33:33 -0400

    revamp for the styling

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ClientComponent } from './client/client.component';
 import { LoginComponent } from './login/login.component';
+import { RecruiterComponent } from './recruiter/recruiter.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ClientComponent,
-    LoginComponent
+    LoginComponent,
+    RecruiterComponent
   ],
   imports: [
     BrowserModule,
