4becbea79f2e815ab7fe77cebdbd3f148c3c01d9
Rudi Rudicel <alexrudicel@comcast.net>
2019-10-17 10:20:30 -0400

    PLEASE GOD WHY ME

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {first} from 'rxjs/operators';
-import {Client} from '../models/client';
+import {Client} from '../models/user';
 import {ClientService} from '../services/client.service';
 import {AuthenticationService} from '../services/authentication.service';
 
