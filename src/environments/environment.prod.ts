26b0a4acb631641c1b27ae92c4b4842dba1332d6
Eric <ejwinebrenner@gmail.com>
2019-10-23 13:46:54 -0400

    client deploy system

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,10 @@
 export const environment = {
   production: true
 };
+
+export let APIURL = '';
+
+switch(window.location.hostname){
+  case 'https://e-recruit-client.herokuapp.com/':
+    APIURL = "https://erecruit-server.herokuapp.com/"
+}
